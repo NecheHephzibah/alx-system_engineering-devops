852b0a23f27c629b685ca23d1a7098d21aed3695
NecheHephzibah <neche.bless@gmail.com>
2024-06-29 10:43:46 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
-# This manifest installs Flask version 2.1.0 using pip3
+# This manifest installs Flask version 2.1.0 and a compatible version of Werkzeug using pip3
 
 package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
   path    => ['/usr/bin', '/usr/sbin'],
   require => Package['python3-pip'],
 }

4f0c13aac9cc94ec257ecd9a71fa790408733502
NecheHephzibah <neche.bless@gmail.com>
2024-07-06 10:49:13 -0700

    SSH connection worked

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-# client SSH configuration file so can connect to a server without a password.
+# Ensure the .ssh directory exists with the correct permissions
+file { '/root/.ssh':
+  ensure  => 'directory',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0700',
+}
+
+# Ensure the SSH config file exists with the correct content and permissions
 file { '/root/.ssh/config':
   ensure  => 'file',
   owner   => 'root',
   group   => 'root',
   mode    => '0600',
-  content => 'Host your_server_alias
-    HostName 35.174.176.31
-    User ubuntu
-    IdentityFile /root/.ssh/school
-    PasswordAuthentication no',
+  content => template('ssh/config.erb'),
 }
-

1e9a7daa228817bb9e07d4f5594cc3f4a76c2e34
NecheHephzibah <neche.bless@gmail.com>
2024-07-06 08:50:47 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,33 @@
-# Installs a Nginx server
+# Ensure the Nginx package is installed
+package { 'nginx':
+  ensure => installed,
+}
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/NecheHephzibah permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start'
+# Ensure the Nginx service is running and enabled to start at boot
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+}
+
+# Create the index page with "Hello World!" content
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  require => Package['nginx'],
+}
+
+# Configure the Nginx server block to include the 301 redirect
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('/alx-system_engineering-devops/0x0C-web_server/nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],  # Notify the Nginx service to restart if this file changes
+}
+
+# Verify Nginx configuration and reload the service if necessary
+exec { 'nginx-reload':
+  command     => '/usr/sbin/nginx -s reload',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
+}

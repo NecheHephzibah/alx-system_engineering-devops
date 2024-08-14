6c08e7eb080eb74c1c17a7664bcd10636c43a236
NecheHephzibah <neche.bless@gmail.com>
2024-08-14 01:06:45 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers,
+for a given subreddit. If an invalid subreddit is given, the function
+should return 0.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns num of subsribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get("data")
+    subscribers = data.get("subscribers")
+
+    return subscribers
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+API advanced. Server will return an XML Document which contains the requested data the documentation describing how the data is organisd spatially
